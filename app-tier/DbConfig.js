6c2febdc624ece64ef3697c5920d074c5c0df47d
saranvas <saran666an@gmail.com>
2025-12-09 13:38:55 +0530

    first commit

---
+++ app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'three-tier-rdsdb.c6ruq64ca225.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'saranvas',
+    DB_DATABASE : 'webappdb'
+});
